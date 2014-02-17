cf254b318598b8e78deb5526a24c81a7f6b3f9f1
Cliff <cliffmin@gmail.com>
2014-02-17 12:44:56 -0700

    added form validation, ng-disabled to submit

--- app/scripts/controllers/NewEvent.js
+++ app/scripts/controllers/NewEvent.js
@@ -4,5 +4,15 @@
  */
 angular.module('example1App')
     .controller('NewEventCtrl', function ($scope) {
-
+        $scope.saveEvent = function (event, newEventForm) {//our event object, properties {name, text, time, location, city, imageUrl}
+            if (newEventForm.$valid) {
+                alert(event.name + " was saved!");
+            }
+            else
+                alert('invalid form!')
+        }
+        $scope.cancelEdit = function () {
+            window.alert('canceled!')
+            window.location = "#/";
+        }
     });
